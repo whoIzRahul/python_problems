df7857452c7917cbb1c20fe9f21fc45264a88460
WhoIzRahul <rookierahul100@gmail.com>
2024-09-08 13:25:04 +0545

    calculating pi approximate

---
+++ pointsInsideCircle.py
@@ -0,0 +1,22 @@
+import random
+
+
+# N = total number of points inside the square
+def calculateApproximateOfPi(N):
+    n = 0  # number of points inside the circle
+    loopCounter = N
+    while loopCounter > 0:
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+        #  (x, y)
+        # checking if this point falls inside the circle using the formula x^2 + y^2 <= 1
+        if x**2 + y**2 <= 1:
+            n += 1
+        loopCounter -= 1
+    print(n, N)
+    print("Approximate value of pi: ", 4 * n / N)
+
+
+pointsInsideSquare = int(input("Enter the number of points inside the square: "))
+
+calculateApproximateOfPi(pointsInsideSquare)
--- smallAndLargeNumbers.py
+++ smallAndLargeNumbers.py
@@ -1,11 +1,12 @@
 smallest = None
 largest = None
 print("Enter a blank line to finish.")
+number = input('enter a number')
 while True:
     n = input("Enter a number: ")
     if n == "":
-        print("The smallest number is:", smallest)
-        print("The largest number is:", largest)
+        print("The smallest number is:" + smallest)
+        print("The largest number is:" + str(largest))
         break
     if smallest == None or float(n) <= smallest:
         smallest = float(n)
